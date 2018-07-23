6683a7e9a6349991ed02c97ed65ef54eceb5708c
Dennis Mende <programming.dmende@hotmail.com>
2018-07-23 21:58:17 +0200

    feat: setup start screen

--- App.js
+++ App.js
@@ -1,23 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+import { green, white } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import DeckOverview from './components/DeckOverview';
+import NewDeck from './components/NewDeck';
+import DeckDetail from './components/DeckDetail';
+
+function FlashCardsStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const TabNavigation = createBottomTabNavigator({
+  DeckOverview: {
+    screen: DeckOverview,
+    navigationOptions: {
+      tabBarLabel: 'Deck Overview',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? green : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : green,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: TabNavigation,
+  },
+  DeckDetail: {
+    screen: DeckDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: green,
+      }
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(rootReducer)}>
+        <View style={{flex: 1}}>
+          <FlashCardsStatusBar backgroundColor={green} barStyle="light-content" />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
