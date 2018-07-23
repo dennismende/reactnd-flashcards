f63752a92e5d64e8a3479692c700b8550444ad3b
Dennis Mende <programming.dmende@hotmail.com>
2018-07-23 21:57:01 +0200

    feat: add color palette

---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const white = '#fff';
+export const yellow = '#fdffc1';
+export const red = '#ff0052';
+export const lightGreen = '#b7ffdb';
+export const brown = '#ffdb9c';
+export const green = '#23f1dd';
